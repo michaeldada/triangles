d1b855d1567b21e354e984d00fb47f9d6e564e16
Joseph Arnstein and Michael Dada <mail.michaeldada@gmail.com>
2016-01-19 13:06:39 -0800

    add final business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
-var isTriangle = function(side1, side2, side3){
+var isTriangle = function(side1, side2, side3) {
 
-if ((side1 === side2) && (side2 === side3) && (side3 === side1)) {
-return "equilateral";
+  if((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1)){
+    if ((side1 === side2) && (side2 === side3) && (side3 === side1)) {
+      return "equilateral";
 
-}else if((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1)){
-return true;
+    } else if (((side1 === side2) && (side1 !== side3)) || ((side1 === side3) && (side1 !== side2)) || ((side3 === side2) && (side3 !== side1))){
+      return "isosceles";
 
-}else {
-return false;
-}
+  //  } else if((side1 !== side2) && (side2 !== side3) && (side3 !== side1)){
+  //    return "scalene";
 
+    } else {
+      return "scalene";
+    }
+    
+  } else {
+    return false;
+  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,12 +1,17 @@
 describe('isTriangle', function(){
   it("is false when the sum of two sides are equal to or less than the third side", function(){
-    expect(isTriangle(25,5,10)).to.equal(false);
+    expect(isTriangle(25,6,5)).to.equal(false);
   });
 
-  it("is true when the sum of two sides are less than the third side", function() {
-    expect(isTriangle(25,35,25)).to.equal(true);
-  });
   it("is 'equalateral' when all sides are equal", function(){
     expect(isTriangle(15,15,15)).to.equal("equilateral")
+  });
+
+  it("is 'isosceles' if exactly two sides are equal", function(){
+    expect(isTriangle(15,15,10)).to.equal("isosceles")
+  });
+
+  it("is 'scalene' if no sides are equal", function(){
+    expect(isTriangle(4,5,6)).to.equal("scalene")
   });
 });
