75d85e67420592bf0580ece0ef3cac9edd1b1321
Joseph Arnstein and Michael Dada <mail.michaeldada@gmail.com>
2016-01-19 13:17:16 -0800

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,16 @@
 var isTriangle = function(side1, side2, side3) {
 
   if((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1)){
-    if ((side1 === side2) && (side2 === side3) && (side3 === side1)) {
+    if ((side1 === side2) && (side2 === side3)) {
       return "equilateral";
 
-    } else if (((side1 === side2) && (side1 !== side3)) || ((side1 === side3) && (side1 !== side2)) || ((side3 === side2) && (side3 !== side1))){
+    } else if ((side1 === side2) || (side1 === side3) || (side3 === side2)){
       return "isosceles";
-
-  //  } else if((side1 !== side2) && (side2 !== side3) && (side3 !== side1)){
-  //    return "scalene";
-
+      
     } else {
       return "scalene";
     }
-    
+
   } else {
     return false;
   }
